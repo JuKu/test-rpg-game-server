7bd46d48be2e28ebcf657e9d6d2708c3e7cd52f6
JuKu <kuenzel.justin@t-online.de>
2017-03-22 15:12:43 +0100

    added maven modules, added main class.

---
+++ game-server/src/main/java/com/jukusoft/libgdx/rpg/game/server/ServerMain.java
@@ -0,0 +1,12 @@
+package com.jukusoft.libgdx.rpg.game.server;
+
+/**
+ * Created by Justin on 22.03.2017.
+ */
+public class ServerMain {
+
+    public static void main (String[] args) {
+        //
+    }
+
+}
