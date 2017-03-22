d7641548bc562aa5bfde63e1446cb9d612ea72fa
JuKu <kuenzel.justin@t-online.de>
2017-03-22 20:54:05 +0100

    added main class for network client.

---
+++ game-network-client/src/main/java/com/jukusoft/libgdx/rpg/game/client/Main.java
@@ -0,0 +1,12 @@
+package com.jukusoft.libgdx.rpg.game.client;
+
+/**
+ * Created by Justin on 22.03.2017.
+ */
+public class Main {
+
+    public static void main (String[] args) {
+        //
+    }
+
+}
