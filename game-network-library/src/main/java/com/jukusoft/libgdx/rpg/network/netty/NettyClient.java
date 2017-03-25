8f480d3d0afab31cb9b62ad2ea98ad861eddc04d
JuKu <kuenzel.justin@t-online.de>
2017-03-25 01:00:23 +0100

    fixed some problems.

--- game-network-library/src/main/java/com/jukusoft/libgdx/rpg/network/netty/NettyClient.java
+++ game-network-library/src/main/java/com/jukusoft/libgdx/rpg/network/netty/NettyClient.java
@@ -111,11 +111,13 @@ public abstract class NettyClient implements Client {
 
     @ChannelHandler.Sharable
     private final class ClientChannelInitializationHandler extends ChannelHandlerAdapter {
+
         @Override
         public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
             //initialize pipeline
             NettyClient.this.initPipeline(ctx.pipeline());
         }
+
     }
 
 }
